72478cf25d3d8ab0e03ff626cff20ac5bf3f752f
Nicolas Romero Niño <77519117+nromeron@users.noreply.github.com>
2025-01-02 20:09:07 -0500

    little changes in the Routes

--- router/menu.js
+++ router/menu.js
@@ -5,10 +5,10 @@ const mdAuth = require("../midellwares/authenticated");
 const api = express.Router();
 
 
-api.post("/menu", [mdAuth.asureAuth] , menuController.createMenu);
-api.get("/menu", menuController.getAllmenus);
-api.patch("/menu/:id", [mdAuth.asureAuth] , menuController.updateMenu);
-api.delete("/menu/:id", [mdAuth.asureAuth], menuController.deleteMenu )
+api.post("/menu/create", [mdAuth.asureAuth] , menuController.createMenu);
+api.get("/menu/all", menuController.getAllmenus);
+api.patch("/menu/update/:id", [mdAuth.asureAuth] , menuController.updateMenu);
+api.delete("/menu/delete/:id", [mdAuth.asureAuth], menuController.deleteMenu )
 
 
 
